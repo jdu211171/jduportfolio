6a185b2d16d6c1e110eb04bbda5ca6c2c865fa85
jdu214843 <214843a@jdu.uz>
2025-03-29 11:54:50 +0500

    Update Layout component structure and styling

--- portfolio-client/src/components/Layout/Layout.jsx
+++ portfolio-client/src/components/Layout/Layout.jsx
@@ -183,8 +183,7 @@ const Layout = () => {
 					<div className={style.topBarBox}>
 						<div className={style.languageSwitcher}>
 							<Notifications />
-						</div>
-						<div className={style.languageSwitcher}>
+							{/*
 							<select
 								onChange={e => changeLanguage(e.target.value)}
 								defaultValue={language}
@@ -193,6 +192,7 @@ const Layout = () => {
 								<option value='en'>English</option>
 								<option value='uz'>O‘zbek</option>
 							</select>
+              */}
 						</div>
 						<div className={style.timeBox}>
 							<div style={{ textAlign: 'right' }}>
