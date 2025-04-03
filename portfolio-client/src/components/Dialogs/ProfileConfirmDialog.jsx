101320e246e7e64cf89c906ef0af93233a7a4a01
jdu211171 <211171m@jdu.uz>
2025-04-03 16:20:44 +0500

    Enhance ProfileConfirmDialog with improved content structure, added dividers, and clarified prohibited actions

--- portfolio-client/src/components/Dialogs/ProfileConfirmDialog.jsx
+++ portfolio-client/src/components/Dialogs/ProfileConfirmDialog.jsx
@@ -7,6 +7,8 @@ import {
 	Typography,
 	Button,
 	Checkbox,
+	Divider,
+	Box
 } from '@mui/material'
 
 function ConfirmationDialog({ open, onClose, onConfirm }) {
@@ -23,25 +25,53 @@ function ConfirmationDialog({ open, onClose, onConfirm }) {
 			<DialogTitle sx={{ fontWeight: '100' }}>公開申請</DialogTitle>
 
 			<DialogContent dividers>
-				{/* Large Title */}
-				<Typography variant='body1' sx={{ fontWeight: 'bold' }}>
-					あなたの情報が承認されるためJDU職員に送信されます
-				</Typography>
-
-				{/* Body text; using whiteSpace: pre-line lets you preserve \n as line breaks */}
+				<Divider sx={{ my: 1 }} />
+				
+				{/* Main content */}
 				<Typography variant='body2' sx={{ whiteSpace: 'pre-line', mt: 1 }}>
-					{`プロフィールの承認依頼はJDU就業部へ送信され、職員による内容確認が行われます。
-不明点や追加の質問がある場合は、メールにてご連絡します。
-問題がなければ承認され、あなたの入力情報や成績情報などがJDU大学のサポート企業に公開され、就職活動がよりスムーズになります。
-プロフィール公開に同意しますか？`}
+					{`就職活動を円滑にする目的のために、あなたのプロフィールを申請に基づき、JDUポー
+トフォリオサービス上に公開します。
+公開申請後はJDU日本就業部に送信され、職員による内容確認が行われます。
+記載内容について不明点や質問がある場合は、メールにてご連絡します。
+記載内容について不明点や質問がない場合は、申請内容が承認され、あなたのプロフ
+ィールが公開されます。
+あなたのプロフィールの内容等が以下の禁止行為に該当しないこと確認し、申請ボタ
+ンから公開申請をしてください。`}
 				</Typography>
-				<Typography>
+				
+				<Divider sx={{ my: 1 }} />
+				
+				{/* Prohibited actions section */}
+				<Typography variant='body2' sx={{ fontWeight: 'bold', mt: 1 }}>
+					（禁止行為）
+				</Typography>
+				<Typography variant='body2' sx={{ whiteSpace: 'pre-line' }}>
+					{`JDU学生は、本サービスの利用に際し、次の各号の行為を行ってはならないものとしま
+す。
+(1)虚偽または不正確な情報を提供する行為
+(2)個人や団体を誹謗、中傷、脅迫、またはそのおそれのある行為
+(3)著作権、商標権、その他の知的財産権を含む他人の権利を侵害し、またはそのおそ
+れのある行為　例：芸能人や他人の写真、作品、キャラクターなどを無断で利用す
+る行為
+(5)本サービスの円滑な運営を妨げる行為、または当社および求人者の信用または名誉
+を毀損する行為、もしくはそれらのおそれのある行為　例：求人者への直接連絡、
+面談等の行為
+(6)犯罪行為、または法令に反する行為等公序良俗に反する行為、もしくはそれらのお
+それのある行為
+(7)反社会的勢力等に関連する組織に属する行為、反社会的勢力に利益を与え、または
+利用する等不適切な関係を持つ行為、もしくはそれらのおそれのある行為`}
+				</Typography>
+				
+				<Divider sx={{ my: 1 }} />
+				
+				{/* Agreement checkbox */}
+				<Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
 					<Checkbox
 						checked={checked}
 						onChange={e => setChecked(e.target.checked)}
 					/>
-					同意
-				</Typography>
+					<Typography>上記の禁止行為に該当しないとことを確認しました</Typography>
+				</Box>
 			</DialogContent>
 
 			<DialogActions sx={{ pr: 3, pb: 2 }}>
