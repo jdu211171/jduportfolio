7b78ab278dfe60c6bbc472d0f5a826b32e33a5ef
Behruz <2310532b@jdu.uz>
2025-04-26 20:21:37 +0500

    beatufy login page is complated

--- portfolio-client/src/components/languageSelector/LanguageSelect.jsx
+++ portfolio-client/src/components/languageSelector/LanguageSelect.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './language.css'
 
-export const LanguageSelect = () => {
+export const LanguageSelect = (props) => {
 	const savedLanguage = localStorage.getItem('language') || 'ja'
 	const [language, setLanguage] = useState(savedLanguage)
 
@@ -16,6 +16,7 @@ export const LanguageSelect = () => {
 			className='languageSelect'
 			onChange={e => changeLanguage(e.target.value)}
 			defaultValue={language}
+			style={props.style} 
 		>
 			<option value='ja'>日本語</option>
 			<option value='en'>English</option>
