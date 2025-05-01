ccb36dc59e4143f129d6550142d151034ea35726
Tillayev Xusniddin <156592821+Tillayevxusniddin@users.noreply.github.com>
2025-05-01 19:06:10 +0500

    Google Login (#105)
    Co-authored-by: devkage <Tillayevxusniddin>

--- portfolio-server/src/services/authService.js
+++ portfolio-server/src/services/authService.js
@@ -1,68 +1,159 @@
+// const bcrypt = require('bcrypt')
+// const jwt = require('jsonwebtoken')
+// const { Admin, Staff, Recruiter, Student } = require('../models')
+
+// class AuthService {
+// 	static async login(email, password, res) {
+// 		const userTypes = [Admin, Staff, Recruiter, Student]
+
+// 		for (const UserType of userTypes) {
+// 			const user = await UserType.findOne({ where: { email } })
+// 			if (user) {
+// 				const isMatch = await bcrypt.compare(password, user.password)
+// 				if (isMatch) {
+// 					const token = jwt.sign(
+// 						{ id: user.id, userType: UserType.name },
+// 						process.env.JWT_SECRET,
+// 						{ expiresIn: process.env.JWT_EXPIRATION }
+// 					)
+
+// 					// Set the JWT token and userType as cookies
+// 					res.cookie('token', token, {
+// 						httpOnly: false,
+// 						secure: process.env.NODE_ENV === 'production',
+// 						expires: new Date(
+// 							Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000
+// 						), // Convert expiresIn to milliseconds
+// 					})
+
+// 					// Also set userType as a separate cookie
+// 					res.cookie('userType', UserType.name, {
+// 						httpOnly: false, // Can be accessed on the client-side
+// 						secure: process.env.NODE_ENV === 'production',
+// 						expires: new Date(
+// 							Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000
+// 						), // Convert expiresIn to milliseconds
+// 					})
+// 					return {
+// 						userType: UserType.name,
+// 						userData: {
+// 							id: user.id,
+// 							name: user.first_name + ' ' + user.last_name,
+// 							studentId: user.student_id,
+// 							photo: user.photo,
+// 						},
+// 					}
+// 				}
+// 			}
+// 		}
+
+// 		throw new Error('Invalid credentials')
+// 	}
+
+// 	static async logout(res) {
+// 		// Clear cookies by setting them to empty and setting expiry in the past
+// 		res.clearCookie('token', {
+// 			httpOnly: false,
+// 			secure: process.env.NODE_ENV === 'production',
+// 			// expires: new Date(0),
+// 		})
+// 		res.clearCookie('userType', {
+// 			httpOnly: false,
+// 			secure: process.env.NODE_ENV === 'production',
+// 			// expires: new Date(0),
+// 		})
+// 	}
+
+// }
+
+// module.exports = AuthService
+
+
+
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { Admin, Staff, Recruiter, Student } = require('../models')
 
 class AuthService {
-	static async login(email, password, res) {
-		const userTypes = [Admin, Staff, Recruiter, Student]
+    static async login(email, password, res) {
+        const userTypes = [Admin, Staff, Recruiter, Student]
 
-		for (const UserType of userTypes) {
-			const user = await UserType.findOne({ where: { email } })
-			if (user) {
-				const isMatch = await bcrypt.compare(password, user.password)
-				if (isMatch) {
-					const token = jwt.sign(
-						{ id: user.id, userType: UserType.name },
-						process.env.JWT_SECRET,
-						{ expiresIn: process.env.JWT_EXPIRATION }
-					)
+        for (const UserType of userTypes) {
+            const user = await UserType.findOne({ where: { email } })
+            if (user) {
+                const isMatch = await bcrypt.compare(password, user.password)
+                if (isMatch) {
+                    const token = jwt.sign(
+                        { id: user.id, userType: UserType.name },
+                        process.env.JWT_SECRET,
+                        { expiresIn: process.env.JWT_EXPIRATION }
+                    )
+                    AuthService.setAuthCookies(res, token, UserType.name)
+                    return {
+                        userType: UserType.name,
+                        userData: {
+                            id: user.id,
+                            name: user.first_name + ' ' + user.last_name,
+                            studentId: user.student_id,
+                            photo: user.photo,
+                        },
+                    }
+                }
+            }
+        }
+        throw new Error('Invalid credentials')
+    }
 
-					// Set the JWT token and userType as cookies
-					res.cookie('token', token, {
-						httpOnly: false,
-						secure: process.env.NODE_ENV === 'production',
-						expires: new Date(
-							Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000
-						), // Convert expiresIn to milliseconds
-					})
+    static async loginWithGoogle(profile) {
+        const email = profile.emails[0].value // Google profilidan emailni olish
+        const userTypes = [Admin, Staff, Recruiter, Student]
 
-					// Also set userType as a separate cookie
-					res.cookie('userType', UserType.name, {
-						httpOnly: false, // Can be accessed on the client-side
-						secure: process.env.NODE_ENV === 'production',
-						expires: new Date(
-							Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000
-						), // Convert expiresIn to milliseconds
-					})
-					return {
-						userType: UserType.name,
-						userData: {
-							id: user.id,
-							name: user.first_name + ' ' + user.last_name,
-							studentId: user.student_id,
-							photo: user.photo,
-						},
-					}
-				}
-			}
-		}
+        for (const UserType of userTypes) {
+            const user = await UserType.findOne({ where: { email } })
+            if (user) {
+                const token = jwt.sign(
+                    { id: user.id, userType: UserType.name },
+                    process.env.JWT_SECRET,
+                    { expiresIn: process.env.JWT_EXPIRATION }
+                )
+                return {
+                    userType: UserType.name,
+                    userData: {
+                        id: user.id,
+                        name: user.first_name + ' ' + user.last_name,
+                        studentId: user.student_id,
+                        photo: user.photo,
+                    },
+                    token,
+                }
+            }
+        }
+        throw new Error('Bu Google email bilan hisob topilmadi')
+    }
 
-		throw new Error('Invalid credentials')
-	}
+    static setAuthCookies(res, token, userType) {
+        res.cookie('token', token, {
+            httpOnly: false,
+            secure: process.env.NODE_ENV === 'production',
+            expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000),
+        })
+        res.cookie('userType', userType, {
+            httpOnly: false,
+            secure: process.env.NODE_ENV === 'production',
+            expires: new Date(Date.now() + parseInt(process.env.JWT_EXPIRATION) * 60 * 60 * 1000),
+        })
+    }
 
-	static async logout(res) {
-		// Clear cookies by setting them to empty and setting expiry in the past
-		res.clearCookie('token', {
-			httpOnly: false,
-			secure: process.env.NODE_ENV === 'production',
-			// expires: new Date(0),
-		})
-		res.clearCookie('userType', {
-			httpOnly: false,
-			secure: process.env.NODE_ENV === 'production',
-			// expires: new Date(0),
-		})
-	}
+    static async logout(res) {
+        res.clearCookie('token', {
+            httpOnly: false,
+            secure: process.env.NODE_ENV === 'production',
+        })
+        res.clearCookie('userType', {
+            httpOnly: false,
+            secure: process.env.NODE_ENV === 'production',
+        })
+    }
 }
 
 module.exports = AuthService
