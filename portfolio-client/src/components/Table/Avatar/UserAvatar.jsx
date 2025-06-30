7a98ddc24cd704622517fb7962984f728b4b8650
jdu211171 <211171m@jdu.uz>
2025-06-30 13:16:06 +0900

    Avatar

---
+++ portfolio-client/src/components/Table/Avatar/UserAvatar.jsx
@@ -0,0 +1,24 @@
+import { Avatar, Chip } from '@mui/material'
+import React from 'react'
+import style from './UserAvatar.module.css'
+
+const UserAvatar = ({ photo, name, studentId }) => {
+	return (
+		<div className={style.avatarContainer}>
+			<Avatar sx={{ width: 40, height: 40 }} alt={name} src={photo} />{' '}
+			<div className={style.nameIdContainer}>
+				<div>{name}</div>
+				{studentId ? (
+					<Chip
+						label={studentId}
+						color='success'
+						variant='outlined'
+						size='small'
+					/>
+				) : null}
+			</div>
+		</div>
+	)
+}
+
+export default UserAvatar
