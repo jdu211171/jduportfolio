9b49c4641565f7c15a10258837b2099f460916cb
Asilbek <122785176+jdu214843@users.noreply.github.com>
2025-07-03 11:46:57 +0900

    Update environment configuration to use development mode (#162)

--- portfolio-server/ecosystem.config.js
+++ portfolio-server/ecosystem.config.js
@@ -1,18 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'portfolio-server',
-    script: './src/app.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 5000
-    },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
-    time: true
-  }]
-};
+	apps: [
+		{
+			name: 'portfolio-server',
+			script: './src/app.js',
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'development',
+				PORT: 5000,
+			},
+			error_file: './logs/err.log',
+			out_file: './logs/out.log',
+			log_file: './logs/combined.log',
+			time: true,
+		},
+	],
+}
