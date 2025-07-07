b4f598afabd5b9d52b8445ac5adaafc7399b9701
jdu214843 <214843a@jdu.uz>
2025-07-07 23:42:14 +0900

    fixed bugs and added new design

--- portfolio-client/vite.config.js
+++ portfolio-client/vite.config.js
@@ -1,33 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-import svgr from '@svgr/rollup';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+import svgr from '@svgr/rollup'
 
-dotenv.config();
+dotenv.config()
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      '@emotion/react', 
-      '@emotion/styled', 
-      '@mui/material/Tooltip'
-    ],
-  },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),svgr()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+	optimizeDeps: {
+		include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
+	},
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: ['@emotion/babel-plugin'],
+			},
+		}),
+		svgr(),
+	],
+	server: {
+		hmr: {
+			overlay: false,
+		},
+		proxy: {
+			'/api': {
+				target: process.env.VITE_API_URL,
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+})
