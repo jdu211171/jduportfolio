18d9e7cbd54337324ea159b93354660f53939a76
Asilbek <122785176+jdu214843@users.noreply.github.com>
2025-07-08 14:56:21 +0900

    fixed bugss (#180)
    done

--- portfolio-client/src/pages/ChekProfile/ChekProfile.jsx
+++ portfolio-client/src/pages/ChekProfile/ChekProfile.jsx
@@ -73,6 +73,8 @@ const Student = ({ OnlyBookmarked = false }) => {
 				t('kyoto_university'),
 				t('otemae_university'),
 				t('niigata_university'),
+				t('sanno_university'),
+				t('forty_credit_model'),
 			],
 			minWidth: '160px',
 		},
